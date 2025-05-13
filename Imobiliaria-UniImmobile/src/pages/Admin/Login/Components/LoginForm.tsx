d0bfd99681292e8e07415767ad526012ef580576
BernardoSsilva <Bernardo.ssilva0808@gmail.com>
2025-05-12 21:23:34 -0300

    :sparkles: initialized login screen form

--- Imobiliaria-UniImmobile/src/pages/Admin/Login/Components/LoginForm.tsx
+++ Imobiliaria-UniImmobile/src/pages/Admin/Login/Components/LoginForm.tsx
@@ -1,24 +1,22 @@
 export function LoginForm() {
     return (
-        <form className="bg-slate-400 p-4 flex-col m-0 h-50% w-20%" >
-            <h1>Realize seu Login</h1>
+        <form className="w-1/2 flex flex-col p-8 py-20  items-center ">
+            <h1>Login</h1>
 
-            <div className="w-full m-1">
-                <label>
-                    Insira seu email:
-                </label>
-                <input type="email" className="border-white-200 border-2" />
-
+            <div>
+                <p>
+                    Informe seu email
+                </p>
+                <input className="border border-solid rounded-s" type="email" />
             </div>
 
-            <div className="w-full">
-                <label>
-                    Insira sua senha:
-                </label>
-                <input type="password" className="border-white-200 border-2 " />
+            <div>
+                <p>
+                    Informe sua senha
+                </p>
+                <input className="border border-solid rounded-s" type="text" />
             </div>
 
-            <input type="submit" className="w-full p-2 text-lg bg-emerald-500 text-white border-r-8" />
         </form>
     )
 }
--- Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
+++ Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
@@ -2,8 +2,11 @@ import { LoginForm } from "./Components/LoginForm";
 
 export function LoginPage() {
     return (
-        <body className="w-full h-full p-36">
+        <div className="inline-flex w-screen overflow-hidden">
+            <div className="bg-gray-100 w-1/2 h-screen">
+                <img className="w-full h-full" src="https://th.bing.com/th/id/OIP.xW2SAK_9PbhzSXaH_7TTEwHaE8?rs=1&pid=ImgDetMain" alt="banana" />
+            </div>
             <LoginForm />
-        </body>
+        </div>
     )
 }
