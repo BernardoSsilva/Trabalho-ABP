fc7ebd980b9e91af4e6bb78dcf39e0848c76f303
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2025-05-26 21:14:38 -0300

    :sparkles: Feat: Login screen layout created

--- Imobiliaria-UniImmobile/src/pages/Admin/Login/Components/LoginForm.tsx
+++ Imobiliaria-UniImmobile/src/pages/Admin/Login/Components/LoginForm.tsx
@@ -1,22 +1,36 @@
+import { useState } from "react"
+
 export function LoginForm() {
+
+    const [rememberMe, setRememberMe] = useState(false)
+
     return (
-        <form className="w-1/2 flex flex-col p-8 py-20  items-center ">
-            <h1>Login</h1>
+        <div className="w-1/2 flex flex-col items-center justify-center">
+            <form className=" w-1/2 h-1/2 p-2 items-center flex flex-col border-gray-300 border-2 rounded-2xl">
+                <h1>Login</h1>
 
-            <div>
-                <p>
-                    Informe seu email
-                </p>
-                <input className="border border-solid rounded-s" type="email" />
-            </div>
+                <div className="mt-5 w-full">
+                    <p>
+                        Informe seu email
+                    </p>
+                    <input className="border border-solid rounded-md w-full" type="email" />
+                </div>
 
-            <div>
-                <p>
-                    Informe sua senha
-                </p>
-                <input className="border border-solid rounded-s" type="text" />
-            </div>
+                <div className="mt-5 w-full">
+                    <p>
+                        Informe sua senha
+                    </p>
+                    <input className="border border-solid rounded-md w-full" type="text" />
+                </div>
+                <div className="flex flex-row w-full">
+                    <input type="checkbox" className="w-4" value={rememberMe.toString()} onChange={(e) => setRememberMe(e.target.value == "true")} />
+                    <p className="ml-2">
+                        lembrar-me
+                    </p>
+                </div>
+                <input className="p-2 px-12 bg-emerald-200 mt-7 text-black hover:bg-emerald-500 hover:text-white transition-colors duration-300 rounded-md" type="submit" value="realizar login" />
+            </form>
 
-        </form>
+        </div>
     )
 }
--- Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
+++ Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "./Components/LoginForm";
 
 export function LoginPage() {
     return (
-        <div className="inline-flex w-screen overflow-hidden">
-            <div className="bg-gray-100 w-1/2 h-screen">
+        <div className="inline-flex w-screen overflow-hidden max-h-screen h-screen">
+            <div className="bg-gray-100 w-1/2">
                 <img className="w-full h-full" src="https://th.bing.com/th/id/OIP.xW2SAK_9PbhzSXaH_7TTEwHaE8?rs=1&pid=ImgDetMain" alt="banana" />
             </div>
             <LoginForm />
