6ae8dbf36d45d448bf0202657bc7c8b6fb3d7fa7
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2025-06-02 20:07:45 -0300

    :bug: bug resolution

--- Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
+++ Imobiliaria-UniImmobile/src/pages/Admin/Login/Login.tsx
@@ -3,7 +3,7 @@ import bgImage from "../../../assets/ImageImmobileLoginScreen.png";
 
 export function LoginPage() {
     return (
-        <div className="inline-flex w-screen overflow-hidden max-h-screen h-screen">
+        <div className="inline-flex w-screen overflow-hidden max-h-screen h-screen m-0 p-0">
             <div className="bg-gray-100 w-1/2">
                 <img src={bgImage} className="w-full h-full" />
             </div>
--- Imobiliaria-UniImmobile/src/pages/UserPage/App.tsx
+++ Imobiliaria-UniImmobile/src/pages/UserPage/App.tsx
@@ -1,6 +1,6 @@
 export function App() {
   return (
-    <main className="bg-gray-700 h-screen w-screen flex justify-center p-2">
+    <main className="bg-gray-700 h-screen w-screen flex justify-center ">
       <h1>
         Configuração inical
       </h1>
